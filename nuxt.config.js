37a15d7527f6303d173f74d4c1c1b210de01540b
Kunal Rajput <kunal.rajput2511@gmail.com>
2022-02-15 00:17:14 +0530

    added robots.txt and uninstalled node sass package

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,14 +24,6 @@ export default {
 
     script: [
       {
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-        body: true,
-      },
-      {
-        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js',
-        body: true,
-      },
-      {
         src: '/main.js',
       },
     ],
