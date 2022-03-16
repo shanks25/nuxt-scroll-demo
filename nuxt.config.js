fd5891d0ff82d3ac38b6cbf1e69609784d2092a3
Kunal Rajput <kunal.rajput2511@gmail.com>
2022-03-16 23:09:03 +0530

    check below comment fixed tag pagination issue added loader wherever needed

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,6 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
